7437de7e2b3ab78cd9f39b6a01b6c20e7c05f98c
noahlin34 <noahlin23@ycdsbk12.ca>
2023-01-11 09:02:17 -0500

    created initial java files

---
+++ src/cpt/HomeTable.java
@@ -0,0 +1,23 @@
+package cpt;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class HomeTable {
+    
+
+
+    
+
+
+}
---
+++ src/cpt/ReadCSV.java
@@ -0,0 +1,22 @@
+package cpt;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
+
+
+
+
+public class ReadCSV {
+
+    public Array[][] read (String file) {
+
+
+
+
+    }
+
+
+    
+}
