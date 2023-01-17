52538be7f2622291c37621a96da696d064a4ae7a
noahlin34 <noahlin23@ycdsbk12.ca>
2023-01-17 09:24:30 -0500

    line chart uses checkboxes to show/hide lines

--- src/cpt/DataLineChart.java
+++ src/cpt/DataLineChart.java
@@ -3,19 +3,29 @@ package cpt;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import javafx.scene.chart.NumberAxis;
 import javafx.stage.Stage;
 import java.util.List;
+
+import javax.swing.Action;
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToolBar;
 
 public final class DataLineChart {
@@ -29,19 +39,30 @@ public final class DataLineChart {
 
     public static Parent makeLineChart() {
 
+
+    
+
+
+
         ToolBar toolbar = new ToolBar();
         toolbar.getItems().add(homeButton);
         toolbar.getItems().add(new Button("Line Chart"));
         toolbar.getItems().add(new Button("Bar Graph"));
         GridPane.setConstraints(toolbar, 0, 0);
 
+        HBox HBox = new HBox();
+        GridPane.setConstraints(HBox, 0, 1);
+        HBox.setSpacing(5);
+        
+        CheckBox australia = new CheckBox("Australia");
 
 
+        HBox.getChildren().addAll(australia);
 
         xAxis = new NumberAxis("Year", 1980, 2020, 1);
         yAxis = new NumberAxis("Gini Coefficient", 20, 40, 1);
-
         lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setAnimated(false);
         String prevCountry = "Australia";
         XYChart.Series<Double, Double> series = new XYChart.Series<>();
 
@@ -50,18 +71,28 @@ public final class DataLineChart {
             if(currentCountry.equals(prevCountry)) {
                 series.getData().add(new XYChart.Data<>(Double.parseDouble(arrData[x][1]), Double.parseDouble(arrData[x][2])));
                 series.setName("AUSTRALIA");
-                lineChart.getData().add(series);
 
-                } else {
+                } else {    
 
             }
-
             
         }
 
-        GridPane.setConstraints(lineChart, 0, 1);
+        australia.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent e) {
+                if(australia.isSelected()) {
+                    lineChart.getData().add(series);
+                } else {
+                    lineChart.getData().remove(series);
+                }
+            }
+        });
+      
 
-        grid.getChildren().addAll(toolbar, lineChart);
+        GridPane.setConstraints(lineChart, 0, 2);
+
+        grid.getChildren().addAll(toolbar, lineChart, HBox);
 
         return grid;
     }
